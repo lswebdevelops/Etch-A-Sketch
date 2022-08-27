6b14df3e687cddb45b4c63ea4c71126bacd9f73c
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-26 22:35:24 -0300

    grid created by a button click

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("hi");
+function myFunction() {
+
+    // Create an "div" node:
+    const node = document.createElement("div");
+    
+    // Create a text node:
+    const textnode = document.createTextNode("Water");
+    
+    // Append the text node to the "li" node:
+    node.appendChild(textnode);
+    
+    // Append the "li" node to the list:
+    document.getElementById("container").appendChild(node);
+    }
+    let i = 0;
+   
+        function myFunction() {
+             while(i <4){
+
+            // Create an "div" node:
+            const node = document.createElement("div");
+            // add style to the div
+            
+            node.setAttribute('style','color: blue; background: yellow;border:solid 1px firebrick; width: 15px; height:15px');
+            // node.setAttribute('style','color: blue; background: yellow;border:solid 1px firebrick; width: 50px; height:50px');
+// 
+            // Create a text node:
+            const textnode = document.createTextNode("");
+            
+            // Append the text node to the "li" node:
+            node.appendChild(textnode);
+            
+            // Append the "li" node to the list:
+            document.getElementById("container").appendChild(node);
+            i++;
+            }
+              console.log(i);
+             
+    }
