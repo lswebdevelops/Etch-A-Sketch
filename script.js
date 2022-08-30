c3dc1a42e140cf1a0372babd1607983ef367066d
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-29 22:07:30 -0300

    math of grid okay

--- script.js
+++ script.js
@@ -1,63 +1,50 @@
+let borderPixels = 1;
+// the grid size asked to the user:
+let pixelSide = prompt("give the side of the grid", 16);
+// the total quantidy of boxes in the grid : side x side
+let totalPixels = pixelSide * pixelSide;
+// the pixels in each box of the grid
+
+// the total  side of the square in pixels: 
+let widthSqr =560;
+// let widthSqr = boxPixels*(pixelSide^(1/2))+borderPixels*2*((pixelSide^(1/2))-1)+(borderPixels*2);
+// the total  side of the square in pixels: 
+let heightSqr = widthSqr;
+let  boxPixels= -((borderPixels*2*((pixelSide^(1/2))-1)+(borderPixels*2) - widthSqr)/ (pixelSide^(1/2)))
+console.log(borderPixels);
+console.log(pixelSide);
+console.log(totalPixels);
+console.log(boxPixels);
+console.log(widthSqr);
+console.log(heightSqr);
+
+// if 16 x 16 clicked, refresh page: 
+function refreshPage(){
+    window .location.reload();   
+    
+}
 // Create an "div" node:
-const nodeDivCont = document.querySelector('#container');
-const nodeBodyCont = document.querySelector('#body');
-
-
+let nodeH2 = document.querySelector('#h2');
 // variable created for the while loops
 let i = 0;    
-// count children of parent node
-
 
 // creates the 16 x 16 grid
-function add16Divs() {
-
-    let temp = document.getElementById('container').childElementCount;
-    console.log(temp);  
-    //if container 64 x 64 was already created, remove it and create a new one.
-    if(temp === 496){
-        nodeDivCont.remove();
-        console.log('nodeDivCont removed');
- // create a div container unter body
- nodeBodyCont.createElement("div");
- nodeBody.id = "container";
-
-        while(i <256){
-           
-
-            // Create an "div" node:
-            const node = document.createElement("div");
-            node.className = "gridDiv";
-            nodeDivCont.className = 'container';          
-            document.getElementById("container").appendChild(node);
-        i++;
-    }     
-    }
-    else{while(i <256){
-        // Create an "div" node:
+function add16Divs() { 
+    document.getElementById('p1').innerText = "16 x 16 grid";
+    // Create an "div" container under H2:    
+    nodeH2 = document.createElement("div");
+        nodeH2.className = "container";
+        nodeH2.id = 'container';          
+        document.getElementById("h2").appendChild(nodeH2);
+    while(i <totalPixels){
+        
+        // Create an "div" under container:
         const node = document.createElement("div");
-        node.className = "gridDiv";
-        nodeDivCont.className = 'container';          
+        node.className = "gridDiv";    
+        node.setAttribute('style', `width:${boxPixels}px; height:${boxPixels}px;`);         
+        // div.setAttribute('style','color: blue; background: white;');
         document.getElementById("container").appendChild(node);
     i++;
-}          
+    }
 }
-                          
-}
-
-
-
-// creates the 64 x 64 grid
-function add64Divs() {
-    let temp = document.getElementById('container').childElementCount;
-    console.log(temp);   
-            while(i <496){
-                     // Create an "div" node:
-                    const node = document.createElement("div");
-                    node.className = "grid64Div";
-                    nodeDivCont.className = 'container';
-                    document.getElementById("container").appendChild(node);
-            i++;
-            }   
-           
-
-        }
+add16Divs()
