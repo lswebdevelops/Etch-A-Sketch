3d8753519f67e361757e1dd0d56af47fcabf1d0a
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-29 23:24:51 -0300

    created a function to change wrong values taken from the users and set them to  1>= value <=64

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let borderPixels = 1;
 // the grid size asked to the user:
-let pixelSide = prompt("give the side of the grid", 16);
+
+let pixelSide = prompt("Choose a grid between 1 and 64", 32);
+
+// takes the max and minimal values to between 1 and 64:
+function handleChange() {
+    if (pixelSide < 1) pixelSide = 1;
+    if (pixelSide> 64) pixelSide= 64;
+  }
+  handleChange()
+  
+let gridSize = document.getElementById('gridSize');
+
+
 // the total quantidy of boxes in the grid : side x side
 let totalPixels = pixelSide * pixelSide;
 // the pixels in each box of the grid
@@ -11,12 +23,12 @@ let widthSqr =560;
 // the total  side of the square in pixels: 
 let heightSqr = widthSqr;
 let  boxPixels= -((borderPixels*2*((pixelSide^(1/2))-1)+(borderPixels*2) - widthSqr)/ (pixelSide^(1/2)))
-console.log(borderPixels);
-console.log(pixelSide);
-console.log(totalPixels);
-console.log(boxPixels);
-console.log(widthSqr);
-console.log(heightSqr);
+// console.log(borderPixels);
+// console.log(pixelSide);
+// console.log(totalPixels);
+// console.log(boxPixels);
+// console.log(widthSqr);
+// console.log(heightSqr);
 
 // if 16 x 16 clicked, refresh page: 
 function refreshPage(){
@@ -30,12 +42,16 @@ let i = 0;
 
 // creates the 16 x 16 grid
 function add16Divs() { 
-    document.getElementById('p1').innerText = "16 x 16 grid";
+    document.getElementById('p1').innerText = `${pixelSide} x ${pixelSide} grid`;
     // Create an "div" container under H2:    
     nodeH2 = document.createElement("div");
         nodeH2.className = "container";
-        nodeH2.id = 'container';          
+        nodeH2.id = 'container';  
+        nodeH2.setAttribute('style', `width:${widthSqr}px;height: ${widthSqr}px;`)        
         document.getElementById("h2").appendChild(nodeH2);
+        // nodeH2.setAttribute('style', 'textAlign: center;')
+        // div.setAttribute('style','color: blue; background: white;');
+
     while(i <totalPixels){
         
         // Create an "div" under container:
